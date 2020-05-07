e7e5d4af0a18322721c4c4ddc80b21ee47a94a2c
Matteo Bettini <43383555+600mt@users.noreply.github.com>
2020-05-07 13:06:47 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,49 +1,81 @@
 $(document).ready(function() {
-    
-$(document).ready(function() {   
-$("#1b, #2a, #2b, #3a, #4a, #4b, #5a, #5b, #5c, #5d, .5e, #6a, .3b, .3c, .3d, .3e, .3f, .3g").hide();
+
+
+$("#1b, #1c, #2a, #2b, #3a, #4a, #4b, #5a, #5b, #5c, #5d, #5e, #6a, .3b, .3c, .3d, .3e, .3f, .3g, #v1, #v2, #v3,#v13, #v4, #v5, #v6, #v7, #v8").hide();
 
 
 $("#1a").click(function(){
 $("#1b").show();
+$("#1a").hide();
+$("#1c").show();
+
 });
-  
+
 
 $("#1b").click(function(){
-	
-  $("#2a, #2b").show();
+    $("#3a, .3b, .3c, .3d, .3e, .3f, .3g").show();
+});
+
+$("#1c").click(function(){
+	$("#1b, #1c, #2a, #2b, #3a, #4a, #4b, #5a, #5b, #5c,  #5d, .5e, #6a, .3b, .3c, .3d, .3e, .3f, .3g, #v1, #v2, #v3,#v13, #v4, #v5, #v6, #v7, #v8").hide();
+    $("#1a").show();
 
 });
 
-$("#2a").click(function(){
-	$("#3a, .3b, .3c, .3d, .3e, .3f, .3g").show();
-
-});
-    
-$("#2b").click(function(){
-	$("#1b, #2a, #2b, #3a, #4a, #4b, #5a, #5b, #5c,  #5d, .5e, #6a, .3b, .3c, .3d, .3e, .3f, .3g").hide();
-
-});
-   
 $("#3a").click(function(){
 	$("#4a, #4b").show();
 
-});   
-    
+});
+
 $("#4a").click(function(){
 	$().show();
 
 });
-    
+
 $("#4b").click(function(){
 	$("#5a, #5b, #5c, #5d, .5e").show();
 
-}); 
+});
 
+
+
+// vignette button
+
+
+
+$("#5a").click(function(){
+	$("#v1, #v4").show();
+    $("#v2, #v3,#v13, #v5, #v6, #v7, #v8").hide();
+
+});
+
+$("#5b").click(function(){
+	$("#v2").show();
+    $("#v1, #v3,#v13, #v4, #v5, #v6, #v7, #v8").hide();
+
+});
+
+$("#5c").click(function(){
+	$("#v3, #v13").show();
+    $("#v1, #v2, #v4, #v5, #v6, #v7").hide();
+
+});
+
+$("#5d").click(function(){
+	$("#v7").show();
+    $("#v1, #v2, #v3,#v13, #v4, #v5, #v6, #v8").hide();
+
+});
+
+$("#5e").click(function(){
+	$("#v5, #v6").show();
+    $("#v1, #v2, #v3,#v13, #v4, #v7, #v8").hide();
+
+});
 // $("#0a").click(function(){
 //	$("#1b, #2a, #2b, #3a, #4a, #4b, #5a, #5b, #5c,  #5d, .5e, #6a, .3b, .3c, .3d, .3e, .3f, .3g").hide();
 
-// }); 
+// });
 
 var flag1=0;
 $("#0a").click(function(){
@@ -59,7 +91,6 @@ $("#0a").click(function(){
 
 });
 
+
+
 });
-    
-});
-    
